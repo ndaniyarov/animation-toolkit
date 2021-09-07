edd6c76e518223dedf5a424ea2ef7c2b4f1edcbf
Nigina Daniyarova <daniyarova.nigina@gmail.com>
2021-09-06 22:51:05 -0400

    Added a red circle and space and r option

--- assignments/a0-start/sphere2d.cpp
+++ assignments/a0-start/sphere2d.cpp
@@ -9,12 +9,18 @@ class Sphere2D : public atkui::Framework {
 
   virtual void scene() {
     // colors are RGB triplets in range [0,1]
-    setColor(vec3(0,1,0));
-
     // draw a sphere at center of screen
     double x = width() * 0.5;
     double y = height() * 0.5;
     double radius = 100; 
+    double radius1 = 200;
+    //red color 
+    setColor(vec3(1,0,0));
+    //draw red sphere
+    drawSphere(vec3(x,y,-100), radius1);
+    //green color
+    setColor(vec3(0,1,0));
+    //draw green sphere in front of red sphere
     drawSphere(vec3(x,y,0), radius);
   }
 };
--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -1,11 +1,17 @@
 #include "atkui/framework.h"
 
-using glm::vec3;
+  using glm::vec3;
 
-class Sphere3D : public atkui::Framework {
- public:
+  class Sphere3D : public atkui::Framework {
+  public:
+
   Sphere3D() : atkui::Framework(atkui::Perspective) {
   }
+
+  vec3 position = vec3(0);
+  vec3 direction = agl::randomUnitVector();
+  vec3 velocity = vec3(100);
+  bool spaceKeyUp = false;
 
   virtual void scene() {
     // colors are RGB triplets in range [0,1]
@@ -13,13 +19,29 @@ class Sphere3D : public atkui::Framework {
 
     // draw a sphere at center of the world
     float radius = 50.0;
-    drawSphere(vec3(0), radius);
+    if (spaceKeyUp){
+      position = position + (velocity * dt() * direction);
+      drawSphere(position, radius);
+    }
+    else {
+      drawSphere(vec3(0), radius);
+    }
+  }
+  void keyUp(int key, int mods) {
+    if (key == GLFW_KEY_SPACE){
+      spaceKeyUp = true;
+    }
+    if (key == GLFW_KEY_R){
+      direction = agl::randomUnitVector();
+      position = vec3(0);
+      spaceKeyUp = false;
+    }
   }
 };
 
 int main(int argc, char** argv)
-{
-  Sphere3D viewer;
-  viewer.run();
-  return 0;
-}
+  {
+    Sphere3D viewer;
+    viewer.run();
+    return 0;
+  }
