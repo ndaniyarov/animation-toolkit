a3573cfe81998d62f5873c31d5cae2ecba451bb9
Nigina Daniyarova <daniyarova.nigina@gmail.com>
2021-10-06 01:51:32 -0400

    submit

--- assignments/a4-euler/unique.cpp
+++ assignments/a4-euler/unique.cpp
@@ -1,7 +1,113 @@
 #include "atkui/framework.h"
-using namespace glm;
+#include "atkmath/constants.h"
+#include "atkmath/vector3.h"
+#include "atkmath/matrix3.h"
 
-int main(int argc, char** argv) {
+using namespace glm;
+using atkmath::Deg2Rad;
+using atkmath::Rad2Deg;
+using atkmath::Matrix3;
+using atkmath::Vector3;
+
+class Circles : public atkui::Framework
+{
+public:
+   Circles() : atkui::Framework(atkui::Orthographic) {
+   }
+   struct particle {
+    vec3 position;
+    vec3 color;
+    float dir;
+    float theta;
+    float radius;
+    float angle;
+  };
+
+   virtual void setup() {
+      k = 0;
+      //number of circles
+      n = 10;
+      //10 degrees in radians
+      thetaRate = 2*M_PI*(10.0f/360.0f);
+      circleR = 10;
+      dir = 1;
+      for (int j = 1.0f; j <= n; j+=1.0f){
+         theta = 0.0f;
+         dir = -1.0 * dir;
+         //evenly distributed circles
+         r = (height()*j)/(2.0f*(n+1.0f));
+         for (int i = 0; i < 36; i++){
+            particle p;
+            p.theta = theta;
+            p.radius = r;
+            p.position.x = r * (cos(p.theta)) + 0.5 * width();
+            p.position.y = r * (sin(p.theta)) + 0.5 * height();
+            p.position.z = 0;
+            p.color = pallet[(j % pallet.size())];
+            p.dir = dir;
+            p.angle = j*Deg2Rad;
+            particles.push_back(p);
+            theta += thetaRate;
+         }
+      }
+   }
+
+   virtual void scene() {
+      
+      for (particle& p : particles) {
+         //p.theta += p.dir * thetaRate * 0.5* dt();
+         theta += thetaRate;
+         //p.position.x = p.radius * (cos(p.theta)) + 0.5 * width();
+         //p.position.y = p.radius * (sin(p.theta)) + 0.5 * height();
+         //translate(vec3(p.position.x, p.position.y, 0.0f));
+         setColor(p.color);
+         //drawCube(p.position, vec3(width()/30.0f));
+         //setColor(vec3(1,0,0));
+         Vector3 euler(0.0001, 0, 0);
+         Matrix3 rot;
+         rot.fromEulerAnglesXYZ(euler*Deg2Rad);
+         glm::mat4 rotm = rot.writeToMat4();
+         Vector3 testXYZ = rot.toEulerAnglesXYZ();
+         testXYZ = testXYZ * Rad2Deg;
+         Vector3 anglesRad = euler* Deg2Rad;
+         push();
+         //translate(vec3(-3.5, 3.5, 0));
+         rotate(anglesRad[0]+theta, vec3(1, 0, 0));
+         rotate(anglesRad[1]+theta, vec3(0, 1, 0));
+         rotate(anglesRad[2]+theta, vec3(0, 0, 1));
+         
+         // rotate(i+M_PI/6.0f, vec3(0,1,0));
+         // rotate(i+M_PI/6.0f, vec3(0,0,1));
+         //drawCone(p.position, circleR);
+         drawTorus(p.position, 10.0);
+         pop();
+         k= k+1;
+      }
+   }
+   private: 
+      float n;
+      float r;
+      float theta;
+      float thetaRate;
+      float circleR;
+      float dir;
+      float k;
+      std::vector<vec3> pallet = 
+      {
+         vec3(0,165,227)/255.0f,
+         vec3(141,215,191)/255.0f,
+         vec3(255,150,197)/255.0f,
+         vec3(255,87,104)/255.0f,
+         vec3(255,162,58)/255.0f
+      };
+      std::vector<particle> particles;
+
+};
+
+int main(int argc, char** argv)
+{
+   Circles viewer;
+   viewer.run();
    return 0;
 }
 
--- libsrc/atkmath/matrix3-basecode.cpp
+++ libsrc/atkmath/matrix3-basecode.cpp
@@ -5,62 +5,299 @@ namespace atkmath {
 
 Vector3 Matrix3::toEulerAnglesXYZ() const
 {
-   return Vector3();
+   float x,y,z;
+   if (m13 == 1 || m13 == -1){
+      if (m13 == 1) {
+         y = M_PI/2.0f;
+         x = atan2(m21,m22);
+      }
+      if (m13 == -1) {
+         y = -M_PI/2.0f;
+         x = atan2(-m21,m22);
+      }
+      z = 0.0f;
+   }
+   else { 
+      y = asin(m13);
+      z = atan2(-m12, m11);
+      x = atan2(-m23, m33);
+   }
+   Vector3 euler = Vector3(x,y,z);
+   return euler;
 }
 
 Vector3 Matrix3::toEulerAnglesXZY() const
 {
-   return Vector3();
+   float x,y,z;
+   if (m12 == 1 || m12 == -1){
+      if (m12 == 1) {
+         z = -M_PI/2.0f;
+         x = atan2(-m31,-m21);
+      }
+      if (m12 == -1) {
+         z = M_PI/2.0f;
+         x = atan2(m31,m21);
+      }
+      y = 0.0f;
+   }
+   else { 
+      z = asin(-m12);
+      y = atan2(m13, m11);
+      x = atan2(m32, m22);
+   }
+   Vector3 euler = Vector3(x,y,z);
+   return euler;
 }
 
+//this might be an issue with 90
 Vector3 Matrix3::toEulerAnglesYXZ() const
 {
-   return Vector3();
+   float x,y,z;
+   if (m23 == 1 || m23 == -1){
+      if (m23 == 1) {
+         x = -M_PI/2.0f;
+         y = atan2(-m12,m11);
+      }
+      if (m23 == -1) {
+         x = M_PI/2.0f;
+         y = atan2(m12,m32);
+      }
+      z = 0.0f;
+   }
+   else { 
+      x = asin(-m23);
+      z = atan2(m21, m22);
+      y = atan2(m13, m33);
+   }
+   Vector3 euler = Vector3(x,y,z);
+   return euler;
 }
 
 Vector3 Matrix3::toEulerAnglesYZX() const
 {
-   return Vector3();
+   float x,y,z;
+   if (m21 == 1 || m21 == -1){
+      if (m21 == 1) {
+         z = M_PI/2.0f;
+         x = atan2(m32,m33);
+      }
+      if (m21 == -1) {
+         z = -M_PI/2.0f;
+         x = atan2(m32,m33);
+      }
+      y = 0.0f;
+   }
+   else { 
+      z = asin(m21);
+      x = atan2(-m23, m22);
+      y = atan2(-m31, m11);
+   }
+   Vector3 euler = Vector3(x,y,z);
+   return euler;
 }
 
 Vector3 Matrix3::toEulerAnglesZXY() const
 {
-   return Vector3();
+   float x,y,z;
+   if (m32 == 1 || m32 == -1){
+      if (m32 == 1) {
+         x = M_PI/2.0f;
+         y = atan2(m21,m11);
+      }
+      if (m32 == -1) {
+         x = -M_PI/2.0f;
+         y = atan2(-m21,m11);
+      }
+      z = 0.0f;
+   }
+   else { 
+      x = asin(m32);
+      y = atan2(-m31, m33);
+      z = atan2(-m12, m22);
+   }
+   Vector3 euler = Vector3(x,y,z);
+   return euler;
 }
 
 Vector3 Matrix3::toEulerAnglesZYX() const
 {
-   return Vector3();
+   float x,y,z;
+   if (m31 == 1 || m31 == -1){
+      if (m31 == 1) {
+         y = -M_PI/2.0f;
+         x = atan2(-m12,-m13);
+      }
+      if (m31 == -1) {
+         y = M_PI/2.0f;
+         x = atan2(m12,m13);
+      }
+      z = 0.0f;
+   }
+   else { 
+      y = asin(-1.0f * m31);
+      z = atan2(m21, m11);
+      x = atan2(m32, m33);
+   }
+   Vector3 euler = Vector3(x,y,z);
+   return euler;
 }
 
 void Matrix3::fromEulerAnglesXYZ(const Vector3& angleRad)
 {
-   *this = Identity;
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   
+   Matrix3 X,Y,Z,XYZ;
+   
+   X.m11 = 1.0f; X.m12 = 0.0f; X.m13 = 0.0f;
+   X.m21 = 0.0f; X.m22 = cos(x); X.m23 = -sin(x);
+   X.m31 = 0.0f; X.m32 = sin(x); X.m33 = cos(x);
+
+   Y.m11 = cos(y); Y.m12 = 0.0f; Y.m13 = sin(y);
+   Y.m21 = 0.0f; Y.m22 = 1.0f; Y.m23 = 0.0f;
+   Y.m31 = -sin(y); Y.m32 = 0.0f; Y.m33 = cos(y);
+
+   Z.m11 = cos(z); Z.m12 = -sin(z); Z.m13 = 0.0f;
+   Z.m21 = sin(z); Z.m22 = cos(z); Z.m23 = 0.0f;
+   Z.m31 = 0.0f; Z.m32 = 0.0f; Z.m33 = 1.0f;
+
+   XYZ = X*Y*Z;
+
+   *this = XYZ;
 }
 
 void Matrix3::fromEulerAnglesXZY(const Vector3& angleRad)
 {
-   *this = Identity;
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   
+   Matrix3 X,Y,Z,XZY;
+   
+   X.m11 = 1.0f; X.m12 = 0.0f; X.m13 = 0.0f;
+   X.m21 = 0.0f; X.m22 = cos(x); X.m23 = -sin(x);
+   X.m31 = 0.0f; X.m32 = sin(x); X.m33 = cos(x);
+
+   Y.m11 = cos(y); Y.m12 = 0.0f; Y.m13 = sin(y);
+   Y.m21 = 0.0f; Y.m22 = 1.0f; Y.m23 = 0.0f;
+   Y.m31 = -sin(y); Y.m32 = 0.0f; Y.m33 = cos(y);
+
+   Z.m11 = cos(z); Z.m12 = -sin(z); Z.m13 = 0.0f;
+   Z.m21 = sin(z); Z.m22 = cos(z); Z.m23 = 0.0f;
+   Z.m31 = 0.0f; Z.m32 = 0.0f; Z.m33 = 1.0f;
+
+   XZY = X*Z*Y;
+
+
+
+   *this = XZY;
 }
 
 void Matrix3::fromEulerAnglesYXZ(const Vector3& angleRad)
 {
-   *this = Identity;
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   
+   Matrix3 X,Y,Z,YXZ;
+   
+   X.m11 = 1.0f; X.m12 = 0.0f; X.m13 = 0.0f;
+   X.m21 = 0.0f; X.m22 = cos(x); X.m23 = -sin(x);
+   X.m31 = 0.0f; X.m32 = sin(x); X.m33 = cos(x);
+
+   Y.m11 = cos(y); Y.m12 = 0.0f; Y.m13 = sin(y);
+   Y.m21 = 0.0f; Y.m22 = 1.0f; Y.m23 = 0.0f;
+   Y.m31 = -sin(y); Y.m32 = 0.0f; Y.m33 = cos(y);
+
+   Z.m11 = cos(z); Z.m12 = -sin(z); Z.m13 = 0.0f;
+   Z.m21 = sin(z); Z.m22 = cos(z); Z.m23 = 0.0f;
+   Z.m31 = 0.0f; Z.m32 = 0.0f; Z.m33 = 1.0f;
+
+   YXZ = Y*X*Z;
+
+
+
+   *this = YXZ;
 }
 
 void Matrix3::fromEulerAnglesYZX(const Vector3& angleRad)
 {
-   *this = Identity;
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   
+   Matrix3 X,Y,Z,YZX;
+   
+   X.m11 = 1.0f; X.m12 = 0.0f; X.m13 = 0.0f;
+   X.m21 = 0.0f; X.m22 = cos(x); X.m23 = -sin(x);
+   X.m31 = 0.0f; X.m32 = sin(x); X.m33 = cos(x);
+
+   Y.m11 = cos(y); Y.m12 = 0.0f; Y.m13 = sin(y);
+   Y.m21 = 0.0f; Y.m22 = 1.0f; Y.m23 = 0.0f;
+   Y.m31 = -sin(y); Y.m32 = 0.0f; Y.m33 = cos(y);
+
+   Z.m11 = cos(z); Z.m12 = -sin(z); Z.m13 = 0.0f;
+   Z.m21 = sin(z); Z.m22 = cos(z); Z.m23 = 0.0f;
+   Z.m31 = 0.0f; Z.m32 = 0.0f; Z.m33 = 1.0f;
+
+   YZX = Y*Z*X;
+
+
+
+   *this = YZX;
 }
 
 void Matrix3::fromEulerAnglesZXY(const Vector3& angleRad)
 {
-   *this = Identity;
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   
+   Matrix3 X,Y,Z,ZXY;
+   
+   X.m11 = 1.0f; X.m12 = 0.0f; X.m13 = 0.0f;
+   X.m21 = 0.0f; X.m22 = cos(x); X.m23 = -sin(x);
+   X.m31 = 0.0f; X.m32 = sin(x); X.m33 = cos(x);
+
+   Y.m11 = cos(y); Y.m12 = 0.0f; Y.m13 = sin(y);
+   Y.m21 = 0.0f; Y.m22 = 1.0f; Y.m23 = 0.0f;
+   Y.m31 = -sin(y); Y.m32 = 0.0f; Y.m33 = cos(y);
+
+   Z.m11 = cos(z); Z.m12 = -sin(z); Z.m13 = 0.0f;
+   Z.m21 = sin(z); Z.m22 = cos(z); Z.m23 = 0.0f;
+   Z.m31 = 0.0f; Z.m32 = 0.0f; Z.m33 = 1.0f;
+
+   ZXY = Z*X*Y;
+
+
+
+   *this = ZXY;
 }
 
 void Matrix3::fromEulerAnglesZYX(const Vector3& angleRad)
 {
-   *this = Identity;
+   float x = angleRad[0];
+   float y = angleRad[1];
+   float z = angleRad[2];
+   
+   Matrix3 X,Y,Z,ZYX;
+   
+   X.m11 = 1.0f; X.m12 = 0.0f; X.m13 = 0.0f;
+   X.m21 = 0.0f; X.m22 = cos(x); X.m23 = -sin(x);
+   X.m31 = 0.0f; X.m32 = sin(x); X.m33 = cos(x);
+
+   Y.m11 = cos(y); Y.m12 = 0.0f; Y.m13 = sin(y);
+   Y.m21 = 0.0f; Y.m22 = 1.0f; Y.m23 = 0.0f;
+   Y.m31 = -sin(y); Y.m32 = 0.0f; Y.m33 = cos(y);
+
+   Z.m11 = cos(z); Z.m12 = -sin(z); Z.m13 = 0.0f;
+   Z.m21 = sin(z); Z.m22 = cos(z); Z.m23 = 0.0f;
+   Z.m31 = 0.0f; Z.m32 = 0.0f; Z.m33 = 1.0f;
+
+   ZYX = Z*Y*X;
+
+   *this = ZYX;
 }
 
 void Matrix3::toAxisAngle(Vector3& axis, double& angleRad) const
